77617f169f48dc5ec322e3e052de64154f9b40f7
Ramindu Jayaweera <ramindujayaweera2000@gmail.com>
2023-11-20 13:26:00 +0530

    Common Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
 import Footer from './components/Footer/Footer';
 import LoginPage from './components/LoginPage/LoginPage';
+import ResetPassword from './components/FogotPassword/ResetPassword';
+import EmailVerify from './components/EmailVerify/EmailVerify';
+import MobileAuthentication from './components/MobileAuthentication/MobileAuthentication';
+import FogotPassword from './components/FogotPassword/FogotPassword';
+import VerifyMN from './components/FogotPassword/VerifyMN';
+import VerifyE from './components/FogotPassword/VerifyE';
+import VerificationPage from './components/Verification/VerificationPage';
+
 
 const App=()=> {
 return (
@@ -12,7 +20,17 @@ return (
 {/* <Navbar />
 <HomePage />
 <Footer /> */}
-<LoginPage/>
+{/* <LoginPage/> */}
+{/* <ResetPassword/> */}
+{/* <EmailVerify/> */}
+
+{/* <FogotPassword/> */}
+{/* <VerifyMN/> */}
+{/* <VerifyE/> */}
+<VerificationPage/>
+
+
+{/* <MobileAuthentication/> */}
     </>
   );
 }                 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,38 @@
 
 import React from 'react'
 import '../Navbar/Navbar.css';
+import GAP_Image from '../Navbar/GAP_BG.png';
+import { Controller } from 'react-hook-form';
 
-const Navbar=()=>{
-    return(
-        <>
-        <div className='custom-containe'>
-        <div className='container'>
-        <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
-        <a className="navbar-brand custom-Logo-link" href="#">Logo</a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link custom-nav-link" href="#">Home</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle custom-nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                For Student
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle custom-nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                For Professional
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link custom-nav-link" href="#">About</a>
-            </li><li className="nav-item">
-              <a className="nav-link custom-nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-          <button class="btn custom-button my-2 my-sm-3 " type="submit">Login</button>
-        </div>
-      </nav></div></div></>
-    )
+const Navbar = () => {
+  return (
+    <>
+      <div className='container'>
+        <nav className="navbar navbar-light">
+          <a className="navbar-brand" href="#">
+            <img src={GAP_Image} height="70" alt="" />
+          </a>
+          <div className="d-flex justify-content-end">
+            <a className="nav-link custom-nav-link " href="#">For Student</a>
+            <a className="nav-link custom-nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              For Student
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+            <a className="nav-link custom-nav-link" href="#">For Professional</a>
+            <a className="nav-link custom-nav-link" href="#">About</a>
+            <a className="nav-link custom-nav-link" href="#">Contact</a>
+            <form className="form-inline my-2 my-lg-0">
+              <button className="btn custom-button1 my-2 my-sm-3" type="submit">Login</button>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </>
+  )
 }
 
 export default Navbar;
